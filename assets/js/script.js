307836484e6c71cfb0aaf47dbd7fdbc929ffd475
xnubar <nxalidova@std.qu.edu.az>
2019-04-09 21:29:00 +0400

    get gifs by api

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+var arr = ["dog", "cat", "frog", "turtle", "goldfish", "bird"];
+
+
+
+//                  FUNCTIONS
+function load() {
+    $(".gif-buttons-container").empty();
+
+    arr.forEach(function (value, index) {
+        let btn = $("<button>");
+        $(btn).addClass("gif-button");
+        $(btn).html
+            (value);
+        $(btn).data("name", value);
+
+        $(".gif-buttons-container").append(btn);
+    })
+
+}
+
+function addNewAnimal(animalName) {
+    if (arr.indexOf(animalName.toLowerCase()) != -1) {
+        return false;
+    } else {
+        arr.add(animalName);
+        return true;
+    }
+}
+
+function loadGifs(images) {
+    /*  <div class="gif-image col-lg-4 col-md-4 col-sm-6 col-xs-6">
+    <h4 class="gif-image-title">Rating</h4>
+    <img src="https://media2.giphy.com/media/1oJlVZu4CDxIKClvbk/giphy.webp" alt=""
+    class="gif-img img-thumbnail">
+    </div >
+    */
+
+    $(".gif-images-container").empty();
+
+    images.forEach(function (value, index) {
+        let div = $("<div>")
+        $(div).addClass("gif-image");
+        $(div).addClass("col-lg-4");
+        $(div).addClass("col-md-4");
+        $(div).addClass("col-sm-6");
+        $(div).addClass("col-xs-6");
+
+        let h4 = $("<h4>");
+        $(h4).addClass("gif-image-title");
+        $(h4).html("Rating: " + value.rating)
+
+        let img = $("<img>");
+        $(img).addClass("gif-img");
+        $(img).addClass("img-thumbnail");
+        $(img).attr("src", value.images.original_still.url)
+        $(img).data("url", value.images.original.url)
+
+        $(div).append(h4);
+        $(div).append(img);
+
+        $(".gif-images-container").append(div);
+    })
+
+
+}
+
+
+
+
+
+//                  EVENTS
+$(document).ready(function () {
+    load();
+})
+
+var queryUrl = "https://api.giphy.com/v1/gifs/search";
+var api_key = "1ERwBbvHtfZhf2nkn1WwIjavf2AXdQn2";
+var limit = 10;
+$(document).on("click", ".gif-button", function () {
+
+    let animal = $(this).data('name');
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+        data: {
+            q: animal,
+            api_key,
+            limit
+        }
+    }).done(function (response) {
+        loadGifs(response.data)
+    })
+
+})
+
+$("form").on("submit", function () {
+
+    let newAnimal = $("#add-animal").val();
+
+    if (newAnimal.length > 1) {
+        let animalIsAdded = addNewAnimal();
+        if (animalIsAdded) {
+            load();
+        } else {
+            //animal already exist in the list => popup
+        }
+    } else {
+        // you can't add  empty string => popup
+    }
+
+})
+
+
+$(document).on("click", ".gif-img", function () {
+    let original=$(this).attr("src");
+    $(this).attr("src",$(this).data("url"));
+    $(this).data("url",original);
+})
